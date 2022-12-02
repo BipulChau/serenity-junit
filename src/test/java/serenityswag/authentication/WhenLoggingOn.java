4d39ef9bcd254fd09d65317d9088f720c3e65e4d
Bipul Chaudhary <bipulchau@gmail.com>
2022-12-02 11:41:50 -0500

    Delete old codes

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -17,21 +17,16 @@ public class WhenLoggingOn extends UIInteractionSteps {
 
     @Test
     public void usersCanLogOnViaTheHomePage(){
-//        driver.get("https://www.saucedemo.com/");
+
         openUrl("https://www.saucedemo.com/");
 
         //Login as a standard user
-
-//        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-//        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-//        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
 
         find("[data-test='username']").sendKeys("standard_user");
         find("[data-test='password']").sendKeys("secret_sauce");
         find("[data-test='login-button']").click();
 
         //should see product catalog
-//        assertThat(driver.findElement(By.cssSelector(".title")).getText()).isEqualToIgnoringCase("Products");
 
         assertThat(find(".title").getText()).isEqualToIgnoringCase("Products");
 
