c48128027a68791ec3d7aaa0d59b3e744aa9ae25
Bipul Chaudhary <bipulchau@gmail.com>
2022-12-02 11:40:12 -0500

    Extends UIInteractionSteps and using find only

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,5 +1,6 @@
 package serenityswag.authentication;
 
+import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
@@ -10,22 +11,30 @@ import org.openqa.selenium.WebDriver;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SerenityRunner.class)
-public class WhenLoggingOn {
+public class WhenLoggingOn extends UIInteractionSteps {
     @Managed
     WebDriver driver;
 
     @Test
     public void usersCanLogOnViaTheHomePage(){
-        driver.get("https://www.saucedemo.com/");
+//        driver.get("https://www.saucedemo.com/");
+        openUrl("https://www.saucedemo.com/");
 
         //Login as a standard user
 
-        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+//        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+//        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+//        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+
+        find("[data-test='username']").sendKeys("standard_user");
+        find("[data-test='password']").sendKeys("secret_sauce");
+        find("[data-test='login-button']").click();
 
         //should see product catalog
-        assertThat(driver.findElement(By.cssSelector(".title")).getText()).isEqualToIgnoringCase("Products");
+//        assertThat(driver.findElement(By.cssSelector(".title")).getText()).isEqualToIgnoringCase("Products");
+
+        assertThat(find(".title").getText()).isEqualToIgnoringCase("Products");
+
     }
 
 }
