06a39994faa0d952aad7fdc1162e97cb2ff8b378
Bipul Chaudhary <bipulchau@gmail.com>
2022-12-03 22:19:38 -0500

    checks if product is displayed in the welcome page by using className

---
+++ src/test/java/serenityswag/inventory/ProductListPageObject.java
@@ -0,0 +1,16 @@
+package serenityswag.inventory;
+
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ProductListPageObject extends PageObject {
+
+    public List<String> titles() {
+        return findAll(By.className("inventory_item_name")).textContents();
+
+
+    }
+}
