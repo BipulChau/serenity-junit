69fa1323b9203aee41056142aaaf3173fddaa808
Bipul Chaudhary <bipulchau@gmail.com>
2022-12-08 22:26:23 -0500

    Create Action Class to refactor

--- src/test/java/actions/LoginActions.java
+++ src/test/java/actions/LoginActions.java
@@ -3,6 +3,7 @@ package actions;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
 import org.openqa.selenium.By;
+import serenityswag.authentication.LoginForm;
 import serenityswag.authentication.User;
 
 public class LoginActions extends UIInteractionSteps {
@@ -10,13 +11,16 @@ public class LoginActions extends UIInteractionSteps {
     public void as(User user){
         openUrl("https://www.saucedemo.com/");
 
-        //Login as a standard user
+//        Login as a standard user
 
-        //$("[data-test='username']").sendKeys(user.getUsername());
-        $("#user-name").sendKeys(user.getUsername());
-        //$("[data-test='password']").sendKeys(user.getPassword());
-        $("#password").sendKeys(user.getPassword());
-        //$("[data-test='login-button']").click();
-        $("#login-button").click();
+//        $("[data-test='username']").sendKeys(user.getUsername());
+//       $("#user-name").sendKeys(user.getUsername());
+       $(LoginForm.USER_NAME).sendKeys(user.getUsername());
+//        $("[data-test='password']").sendKeys(user.getPassword());
+//        $("#password").sendKeys(user.getPassword());
+        $(LoginForm.PASSWORD).sendKeys(user.getPassword());
+//        $("[data-test='login-button']").click();
+//        $("#login-button").click();
+        $(LoginForm.LOGIN_BUTTON).click();
     }
 }
